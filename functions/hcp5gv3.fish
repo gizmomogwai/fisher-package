e856749e3af8d8e87a9fe1229d1dfc581763a9c5
Christian Koestlin <christian.koestlin@esrlabs.com>
2020-08-13 23:22:40 +0200

    Align hcp5 and hcp5gv3 scripts

--- functions/hcp5gv3.fish
+++ functions/hcp5gv3.fish
@@ -11,12 +11,13 @@ function hcp5gv3 --description "HCP5-GV3 Project setup"
 
   set -gx WS /media/more/hcp5/ws/gv3
   cd $WS
-  rvm ruby-2.6.0@hcp5
+  rvm ruby-2.7.1@hcp5
   
   set -gx Target audi_hcp5_bosch
   set -gx Debug true
 
-  functions -c fish_prompt _old_hcp5_fish_prompt
+  functions --erase _old_hcp5gv3_fish_prompt
+  functions -c fish_prompt _old_hcp5gv3_fish_prompt
   function fish_prompt
     set -l bg 333
     set -l standout 0f0
@@ -31,6 +32,7 @@ function hcp5gv3 --description "HCP5-GV3 Project setup"
     echo -n Debug=
     set_color $standout --background $bg
     echo $Debug
-    _old_hcp5_fish_prompt
+
+    _old_hcp5gv3_fish_prompt
   end
 end
