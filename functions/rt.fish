5714418ff1822e497261ebe85425500efe18be65
Christian Koestlin <christian.koestlin@esrlabs.com>
2021-01-18 00:21:10 +0100

    Allow multiple selection in rt

--- functions/rt.fish
+++ functions/rt.fish
@@ -1,5 +1,5 @@
 function rt -d "select one rake task"
-  if rake -T | fzf --ansi > /tmp/fzf.result
+  if rake -T | fzf --ansi --multi > /tmp/fzf.result
     set fzf_result (cat /tmp/fzf.result | cut -d ' ' -f 2)
     set h "rake $fzf_result"
     commandline $h
