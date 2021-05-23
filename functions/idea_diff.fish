71dfe0638cc001c7c5cb334095e64eaf47ff898c
Christian Koestlin <christian.koestlin@gmail.com>
2021-05-23 13:35:19 +0200

    Implement idea_diff also for linux

--- functions/idea_diff.fish
+++ functions/idea_diff.fish
@@ -1,3 +1,10 @@
 function idea_diff -d 'idea diff'
-  ~/Library/Application\ Support/JetBrains/Toolbox/apps/IDEA-C/ch-0/211.7142.45/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea diff diff $argv
+    switch (uname)
+        case Linux
+            ~/.local/share/JetBrains/Toolbox/apps/IDEA-C/ch-0/*/bin/idea.sh diff $argv
+        case Darwin
+            ~/Library/Application\ Support/JetBrains/Toolbox/apps/IDEA-C/ch-0/*/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea diff $argv
+        case "*"
+            echo "unknown os"
+    end
 end
