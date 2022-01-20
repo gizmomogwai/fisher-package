36f397cd1081da5177e1dfb997dd9bf588e06e6b
Christian Koestlin <christian.koestlin@gmail.com>
2022-01-20 17:52:53 +0100

    Start dlang-news proxy in /tmp

--- functions/dlang-news.fish
+++ functions/dlang-news.fish
@@ -1,4 +1,4 @@
 function dlang-news -d 'Read dlang news in thunderbird'
-  ~/bin/nntp-proxy-digitalmars-com &
+  cd /tmp && ~/bin/nntp-proxy-digitalmars-com &
   open -a Thunderbird
 end
--- functions/get-torrent.fish
+++ functions/get-torrent.fish
@@ -1,13 +1,15 @@
 function get-torrent -d "get a torrent file via deluge"
+  set name $argv[1]
+  set torrent $argv[2]
   set location (/Sy*/L*/Priv*/Apple8*/V*/C*/R*/airport -I | awk '/ SSID:/ {print $2}')
   switch $location
     case "Lakeside"
-      set h pi@seehaus-piano.local
+      set h seehaus-piano.local
     case "Pepe"
       set h pi@fs.local
     case "*"
       echo "Unknown location: $location"
       exit 1
   end
-  ssh $h "sudo ip netns exec protected su -l pi -c '/usr/bin/deluge-console add $argv[1]'"
+  ssh $h ./get-torrent.sh $name "$torrent"
 end
