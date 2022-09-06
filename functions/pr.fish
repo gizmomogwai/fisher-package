7e051f5c0b099ada71b20054c0db2a23796bfba6
christian.koestlin <christian.koestlin@gmail.com>
2022-09-06 16:28:51 +0200

    fix: Fix typo

--- functions/pr.fish
+++ functions/pr.fish
@@ -1,6 +1,6 @@
 function pr -d "list all projects with projects.rb"
   set selectedDir (~/Sync/projects/projects.rb | fzf)
   if test $status -eq 0
-    pushd $selectedDIr
+    pushd $selectedDir
   end
 end
