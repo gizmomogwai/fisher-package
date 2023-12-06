54887d926b832294deacc06d98fb50c8e11702dc
Christian Koestlin <christian.koestlin@gmail.com>
2023-12-06 11:31:10 +0100

    feature: Adjust path to idea

--- functions/idea_diff.fish
+++ functions/idea_diff.fish
@@ -1,7 +1,7 @@
 function idea_diff -d 'idea diff'
     switch (uname)
         case Linux
-            ~/.local/share/JetBrains/Toolbox/apps/IDEA-C/ch-0/*/bin/idea.sh diff $argv
+            ~/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh diff $argv
         case Darwin
             ~/Library/Application\ Support/JetBrains/Toolbox/apps/IDEA-C/ch-0/*/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea diff $argv
         case "*"
