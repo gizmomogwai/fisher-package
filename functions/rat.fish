1ed1e5e09f19452f18f22c43e5c4d53dec43ace5
Christian Köstlin <christian.koestlin@gmail.com>
2024-01-16 13:59:42 +0100

    feature: Implement rake --tasks --all aka rat

--- functions/rat.fish
+++ functions/rat.fish
@@ -1,5 +1,5 @@
-function rt -d "select one rake task"
-  if rake --tasks | fzf --ansi --multi > /tmp/fzf.result
+function rat -d "select one rake task"
+  if rake --all --tasks | fzf --ansi --multi > /tmp/fzf.result
     set fzf_result (cat /tmp/fzf.result | cut -d ' ' -f 2)
     set h "rake $fzf_result"
     commandline $h
