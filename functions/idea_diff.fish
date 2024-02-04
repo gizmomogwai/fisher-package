fac0449c6e77afb211b5e2db019cb26ac5a6c0c8
Christian Köstlin <christian.koestlin@gmail.com>
2024-02-04 21:29:37 +0100

    fix: Fix path to intellij on os x

--- functions/idea_diff.fish
+++ functions/idea_diff.fish
@@ -3,7 +3,7 @@ function idea_diff -d 'idea diff'
         case Linux
             ~/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh diff $argv
         case Darwin
-            ~/Library/Application\ Support/JetBrains/Toolbox/apps/IDEA-C/ch-0/*/IntelliJ\ IDEA\ CE.app/Contents/MacOS/idea diff $argv
+            ~/Applications/IntelliJ\ IDEA\ Community\ Edition.app/Contents/MacOS/idea diff $argv
         case "*"
             echo "unknown os"
     end
