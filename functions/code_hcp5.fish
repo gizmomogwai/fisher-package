45517e7d1e14f76edc515af15e8fc873b5c374f9
Christian Köstlin <christian.koestlin@gmail.com>
2024-07-09 20:50:30 +0200

    Add vs-code function for hcp5

---
+++ functions/code_hcp5.fish
@@ -0,0 +1,3 @@
+function code_hcp5 --description "Open code on a esr-desktop hcp5 folder"
+  code --folder-uri vscode-remote://ssh-remote+esr-desktop/home/christian-koestlin/ESRLabs/HCP5/audi-hcp5-gen$argv[1]/
+end
